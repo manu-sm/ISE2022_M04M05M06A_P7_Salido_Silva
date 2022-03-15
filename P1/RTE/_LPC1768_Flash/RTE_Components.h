cde07581b139cbc1c11e1994ba7cf97c4251d27a
Manuel Silva <manuel.silvam@alumnos.upm.es>
2022-03-15 12:12:41 +0100

    Commit inicial

---
+++ P1/RTE/_LPC1768_Flash/RTE_Components.h
@@ -0,0 +1,45 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'HTTP_Server' 
+ * Target:  'LPC1768 Flash' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "LPC17xx.h"
+
+/* ARM::CMSIS:RTOS:Keil RTX:4.82.0 */
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+/* Keil.MDK-Pro Net_v6::Network:CORE:Release:6.7.7 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_Release             /* Network Release Version */
+/* Keil.MDK-Pro Net_v6::Network:Interface:ETH:6.7.7 */
+#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
+
+/* Keil.MDK-Pro Net_v6::Network:Service:Web Server Compact:6.7.7 */
+#define RTE_Network_Web_Server_RO       /* Network Web Server with Read-only Web Resources */
+/* Keil.MDK-Pro Net_v6::Network:Socket:TCP:6.7.7 */
+#define RTE_Network_Socket_TCP          /* Network Socket TCP */
+/* Keil.MDK-Pro Net_v6::Network:Socket:UDP:6.7.7 */
+#define RTE_Network_Socket_UDP          /* Network Socket UDP */
+/* Keil::CMSIS Driver:Ethernet MAC:2.11.0 */
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+/* Keil::CMSIS Driver:Ethernet PHY:DP83848C:6.2.0 */
+#define RTE_Drivers_PHY_DP83848C        /* Driver PHY DP83848C */
+/* Keil::CMSIS Driver:SPI:SSP:2.7.0 */
+#define RTE_Drivers_SPI0                /* Driver SPI0 */
+        #define RTE_Drivers_SPI1                /* Driver SPI1 */
+/* Keil::Device:Startup:1.0.0 */
+#define RTE_DEVICE_STARTUP_LPC17XX      /* Device Startup for NXP17XX */
+
+
+#endif /* RTE_COMPONENTS_H */
